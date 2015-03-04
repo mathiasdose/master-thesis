d1ef3231031d8d3e68507c82002a544cb035f3a3
Mathias Dosé <mathias.dose@hotmail.se>
2015-03-04 12:57:42 +0100

    Added the JSON serializer test type

---
+++ express/app.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var app = module.exports = express();
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
+var server = require('http').createServer(app);
+
+
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(methodOverride());
+
+
+app.get('/json', function (req, res) {
+  res.send({ message: 'Hello, World!' })
+});
+
+server.listen(9000, function () {
+  console.log('Express server listening on %d', 9000);
+});
