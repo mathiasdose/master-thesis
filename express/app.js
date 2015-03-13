1812288fb718dd110b43ae778de661a7751ce5de
Mathias Dosé <mathias.dose@hotmail.se>
2015-03-13 13:12:30 +0100

    clustered

--- express/app.js
+++ express/app.js
@@ -1,8 +1,9 @@
+var cluster = require('cluster');
+var numCPUs = require('os').cpus().length;
 var express = require('express');
-var app = module.exports = express();
 var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
-var server = require('http').createServer(app);
+var http = require('http');
 
 
 
@@ -10,11 +11,26 @@ var server = require('http').createServer(app);
 //app.use(bodyParser.json());
 //app.use(methodOverride());
 
+if (cluster.isMaster) {
+  // Fork workers.
+  for (var i = 0; i < numCPUs; i++) {
+    cluster.fork();
+  }
 
-app.get('/json', function (req, res) {
-  res.send({ message: 'Hello, World!' })
-});
+  cluster.on('exit', function(worker, code, signal) {
+    console.log('worker ' + worker.pid + ' died');
+  });
+} else {
+  var app = module.exports = express();
+  var server = http.createServer(app);
+  app.get('/json', function (req, res) {
+    res.send({ message: 'Hello, World!' })
+  });
 
-server.listen(9000, function () {
-  console.log('Express server listening on %d', 9000);
-});
+  server.listen(9000, function () {
+    console.log('Express server listening on %d', 9000);
+  });
+
+}
+
+
