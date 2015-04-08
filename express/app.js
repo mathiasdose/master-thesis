d728c3b2a3108987c353b029452a5b461cee9cd2
Mathias Dosé <mathias.dose@hotmail.se>
2015-04-08 13:34:41 +0200

    Now creates routes depending on -r

--- express/app.js
+++ express/app.js
@@ -4,8 +4,14 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
 var http = require('http');
+var program = require('commander');
+var uuid = require('node-uuid');
 
 
+program
+  .version('0.0.1')
+  .option('-r, --routes <n>', 'Number of host to deploy', parseInt)
+  .parse(process.argv);
 
 //app.use(bodyParser.urlencoded({ extended: false }));
 //app.use(bodyParser.json());
@@ -23,9 +29,24 @@ if (cluster.isMaster) {
 } else {
   var app = module.exports = express();
   var server = http.createServer(app);
-  app.get('/json', function (req, res) {
-    res.send({ message: 'Hello, World!' })
-  });
+
+
+  var routes = program.routes === undefined ? 1: program.routes;
+  var methods = ['get', 'post', 'put', 'delete'];
+  var div = routes/methods.length;
+  console.log(methods[Math.floor(100000 / div)]);
+  for(var i = 0; i < routes; i++) {
+    if (i === routes/2) {
+      app.get('/json/hello/:id/world', function (req, res) {
+        res.send();
+      });
+    } else {
+      var method = methods[Math.floor(i / div)];
+      app[method]('/json/' + uuid.v4() + '/:id/' + uuid.v4(), function (req, res) {
+        res.send();
+      });
+    }
+  }
 
   server.listen(9000, function () {
     console.log('Express server listening on %d', 9000);
