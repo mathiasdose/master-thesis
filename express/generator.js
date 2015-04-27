4668f647b737a7dc44d8f7700ccd558b195d2f35
Mathias Dosé <mathias.dose@hotmail.se>
2015-04-27 14:15:39 +0200

    Finished generator.js and serialization.js

--- express/generator.js
+++ express/generator.js
@@ -1,8 +1,8 @@
 'use strict';
 var _ = require('lodash');
 var program = require('commander');
-var fs = require("fs");
-
+var Promise = require('bluebird');
+var fs = Promise.promisifyAll(require("fs"));
 program
   .version('0.0.1')
   .usage('[options]')
@@ -13,10 +13,6 @@ program
 var DIRECTORY = './data/';
 var FILETYPE = '.json';
 
-var jsonToWrite = [];
-
-console.log(getFilesizeInBytes('10'));
-
 var sizeInKB;
 if (program.size) {
   sizeInKB = program.size;
@@ -25,16 +21,7 @@ if (program.size) {
   process.exit(1);
 }
 
-initiateFile(program.size, jsonToWrite);
-
-var index = 0;
-/*
-while(true) {
-  var jsonIter = getJSON(index);
-  index++;
-  break;
-}*/
-
+writeJsonToFile(sizeInKB);
 
 
 function getFilesizeInBytes(filename) {
@@ -42,26 +29,42 @@ function getFilesizeInBytes(filename) {
   return stats.size
 }
 
-function initiateFile(filename) {
-  //var emptyArray = JSON.stringify([]);
+function writeJsonToFile(filename) {
   var path = DIRECTORY + filename + FILETYPE;
-  console.log(path);
-  fs.writeSync(path, "YOLO");
+  var json = [];
+
+  var size = 0;
+  var counter = 0;
+  for (var i = 0; i < sizeInKB*1000/205; i++) {
+    json.push(getJSON(i));
+  }
+  var fd = fs.openSync(path, 'w');
+  fs.writeSync(fd, JSON.stringify(json));
+
+ /* while (size < sizeInKB * 1000) {
+    json.push(getJSON(counter));
+    var fd = fs.openSync(path, 'w');
+    fs.writeSync(fd, JSON.stringify(json));
+    counter++;
+    size = getFilesizeInBytes(filename);
+  }*/
+  fs.closeSync(fd);
 }
 
 
 function getJSON(index) {
   return {
-      Id: index,
-      aNull: null,
-      aBoolean: true,
-      anInteger: 68523,
-      aFloat: 6.8523015e+5,
-      aString: 'Testing s',
-      anArray: [null, true, 123, "any"],
-      anObject: { aNull: null, aBoolean: true, anInteger: 123, aString: 'any' }
-    };
+    Id: index,
+    aNull: null,
+    aBoolean: true,
+    anInteger: 68523,
+    aFloat: 6.8523015e+5,
+    aString: 'Testing s',
+    anArray: [null, true, 123, "any"],
+    anObject: { aNull: null, aBoolean: true, anInteger: 123, aString: 'any' }
+  };
 }
 
+console.log('Wrote ' + getFilesizeInBytes(sizeInKB) + 'bytes to ' + DIRECTORY + sizeInKB + FILETYPE);
   
 
